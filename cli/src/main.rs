8f3fc37edba5bf72bd633e7e287436ac92b3558e
Catherine <whitequark@whitequark.org>
2025-02-03 19:21:34 +0000

    Implement IR reading and writing in CLI with `.uir` extension.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,31 @@
-use std::{collections::BTreeMap, fs::File};
+use std::{collections::BTreeMap, fs::File, io::Write};
 
-use prjunnamed_generic::{canonicalize, lower};
+use prjunnamed_generic::{canonicalize, decision, lower};
+use prjunnamed_netlist::{CellRepr, Design};
+
+fn process(name: &str, design: &mut Design) {
+    print!("; {} (initial)\n{:#}\n", name, design);
+
+    match design.target() {
+        None => {
+            decision(design);
+            canonicalize(design);
+            lower(design);
+            canonicalize(design);
+        }
+        Some(ref target) => {
+            for cell_ref in design.iter_cells() {
+                if let CellRepr::Name(_, _) = &*cell_ref.repr() {
+                    cell_ref.unalive();
+                }
+            }
+            design.apply();
+            target.synthesize(design).expect("synthesis failed")
+        }
+    }
+
+    print!("; {} (final)\n{}\n", name, design);
+}
 
 fn run() -> Result<(), Box<dyn std::error::Error>> {
     prjunnamed_siliconblue::register();
@@ -10,9 +35,9 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
     let mut target = None::<String>;
     {
         let mut parser = argparse::ArgumentParser::new();
-        parser.refer(&mut input).add_argument("INPUT", argparse::Store, "Input JSON");
-        parser.refer(&mut output).add_argument("OUTPUT", argparse::Store, "Output JSON");
         parser.refer(&mut target).add_option(&["-t", "--target"], argparse::StoreOption, "Target platform");
+        parser.refer(&mut input).add_argument("INPUT", argparse::Store, "Input file");
+        parser.refer(&mut output).add_argument("OUTPUT", argparse::Store, "Output file");
         parser.parse_args_or_exit();
     }
 
@@ -20,25 +45,35 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
         prjunnamed_netlist::create_target(name.as_str(), BTreeMap::new()).unwrap_or_else(|err| panic!("{}", err))
     });
 
-    let mut design_bundle = prjunnamed_yosys_json::import(target.clone(), &mut File::open(input)?)?;
-
-    for (name, design) in design_bundle.iter_mut() {
-        print!("; {} (initial)\n{:#}\n", name, design);
-
-        match target {
-            Some(ref target) => target.synthesize(design).expect("synthesis failed"),
-            None => {
-                canonicalize(design);
-                lower(design);
-                canonicalize(design);
+    if input.ends_with(".uir") {
+        let mut design = prjunnamed_netlist::parse(target, &std::fs::read_to_string(input)?)?;
+        process("top", &mut design);
+        if !output.is_empty() {
+            if output.ends_with(".uir") {
+                write!(&mut File::create(output)?, "{design}")?;
+            } else if output.ends_with(".json") {
+                prjunnamed_yosys_json::export(
+                    &mut File::create(output)?,
+                    BTreeMap::from_iter([("top".to_owned(), design)]),
+                )?;
+            } else {
+                panic!("don't know what to do with output {output:?}")
             }
         }
-
-        print!("; {} (final)\n{}\n", name, design);
-    }
-
-    if !output.is_empty() {
-        prjunnamed_yosys_json::export(&mut File::create(output)?, design_bundle)?;
+    } else if input.ends_with(".json") {
+        let mut design_bundle = prjunnamed_yosys_json::import(target.clone(), &mut File::open(input)?)?;
+        for (name, design) in design_bundle.iter_mut() {
+            process(name, design);
+        }
+        if !output.is_empty() {
+            if output.ends_with(".json") {
+                prjunnamed_yosys_json::export(&mut File::create(output)?, design_bundle)?;
+            } else {
+                panic!("don't know what to do with output {output:?}")
+            }
+        }
+    } else {
+        panic!("don't know what to do with input {input:?}")
     }
 
     Ok(())
