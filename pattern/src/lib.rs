336dfb342bb42a097f0afbce1be107029041c713
Maja Kądziołka <maya@compilercrim.es>
2025-02-10 22:53:18 +0100

    pattern: Use $crate where possible

--- pattern/src/lib.rs
+++ pattern/src/lib.rs
@@ -7,15 +7,15 @@ pub trait Pattern<Target> {
 #[macro_export]
 macro_rules! netlist_matches {
     { [ $($rule:tt)* ] $($rest:tt)* } => {
-        |design: &dyn prjunnamed_pattern::DesignDyn, target: &prjunnamed_netlist::Value| {
-            prjunnamed_pattern::netlist_matches! { @TOP@ design target [ $($rule)* ] $($rest)* }
+        |design: &dyn $crate::DesignDyn, target: &prjunnamed_netlist::Value| {
+            $crate::netlist_matches! { @TOP@ design target [ $($rule)* ] $($rest)* }
         }
     };
     { @TOP@ $design:ident $target:ident $($rest:tt)* } => {
         {
             if $target.len() > 0 {
-                use prjunnamed_pattern::Pattern;
-                prjunnamed_pattern::netlist_matches! { @RULE@ $design $target $($rest)* }
+                use $crate::Pattern;
+                $crate::netlist_matches! { @RULE@ $design $target $($rest)* }
             } else {
                 None
             }
@@ -25,9 +25,9 @@ macro_rules! netlist_matches {
     { @RULE@ $design:ident $target:ident [ $($pat:tt)+ ] $( if $guard:expr )? => $result:expr; $($rest:tt)* } => {
         {
             'block: {
-                let pattern = prjunnamed_pattern::netlist_matches!( @NEW@ [ $($pat)+ ] );
+                let pattern = $crate::netlist_matches!( @NEW@ [ $($pat)+ ] );
                 match pattern.execute($design, $target) {
-                    Some(prjunnamed_pattern::netlist_matches!( @PAT@ [ $($pat)+ ] )) $( if $guard )? => {
+                    Some($crate::netlist_matches!( @PAT@ [ $($pat)+ ] )) $( if $guard )? => {
                         if cfg!(feature = "trace") {
                             eprintln!(">match {} => {}",
                                 stringify!([ $($pat)* ] $( if $guard )?).replace("\n", " "),
@@ -38,16 +38,16 @@ macro_rules! netlist_matches {
                     }
                     _ => ()
                 }
-                prjunnamed_pattern::netlist_matches! { @RULE@ $design $target $($rest)* }
+                $crate::netlist_matches! { @RULE@ $design $target $($rest)* }
             }
         }
     };
     { @RULE@ $design:ident $target:ident [ $($pat:tt)+ ] if let $gpat:pat = $gexpr:expr => $result:expr; $($rest:tt)* } => {
         {
             'block: {
-                let pattern = prjunnamed_pattern::netlist_matches!( @NEW@ [ $($pat)+ ] );
+                let pattern = $crate::netlist_matches!( @NEW@ [ $($pat)+ ] );
                 match pattern.execute($design, $target) {
-                    Some(prjunnamed_pattern::netlist_matches!( @PAT@ [ $($pat)+ ] )) => {
+                    Some($crate::netlist_matches!( @PAT@ [ $($pat)+ ] )) => {
                         if let $gpat = $gexpr {
                             if cfg!(feature = "trace") {
                                 eprintln!(">match {} => {}",
@@ -60,33 +60,33 @@ macro_rules! netlist_matches {
                     }
                     _ => ()
                 }
-                prjunnamed_pattern::netlist_matches! { @RULE@ $design $target $($rest)* }
+                $crate::netlist_matches! { @RULE@ $design $target $($rest)* }
             }
         }
     };
     ( @NEW@ [ $pat:ident $( @ $cap:ident )? $( ( $($exparg:tt)+ ) )+ $( [ $($patarg:tt)+ ] )* ] ) => {
-        $pat::new( $( $($exparg)+ ),+ , $( prjunnamed_pattern::netlist_matches!( @NEW@ [ $($patarg)+ ] ) ),*)
+        $pat::new( $( $($exparg)+ ),+ , $( $crate::netlist_matches!( @NEW@ [ $($patarg)+ ] ) ),*)
     };
     ( @NEW@ [ $pat:ident $( @ $cap:ident )? $( [ $($patarg:tt)+ ] )* ] ) => {
-        $pat::new( $( prjunnamed_pattern::netlist_matches!( @NEW@ [ $($patarg)+ ] ) ),*)
+        $pat::new( $( $crate::netlist_matches!( @NEW@ [ $($patarg)+ ] ) ),*)
     };
     ( @PAT@ [ $pat:ident $( ( $($exparg:tt)+ ) )* $( [ $($patarg:tt)+ ] )* ] ) => {
-        (_, $( prjunnamed_pattern::netlist_matches!( @PAT@ [ $($patarg)+ ] ) ),*)
+        (_, $( $crate::netlist_matches!( @PAT@ [ $($patarg)+ ] ) ),*)
     };
     ( @PAT@ [ $pat:ident @ $cap:ident $( ( $($exparg:tt)+ ) )* $( [ $($patarg:tt)+ ] )* ] ) => {
-        ($cap, $( prjunnamed_pattern::netlist_matches!( @PAT@ [ $($patarg)+ ] ) ),*)
+        ($cap, $( $crate::netlist_matches!( @PAT@ [ $($patarg)+ ] ) ),*)
     };
 }
 
 #[macro_export]
 macro_rules! netlist_replace {
     { [ $($rule:tt)* ] $($rest:tt)* } => {
-        |design: &dyn prjunnamed_pattern::DesignDyn, target: &prjunnamed_netlist::Value| -> bool {
-            prjunnamed_pattern::netlist_replace! { @TOP@ design target [ $($rule)* ] $($rest)* }
+        |design: &dyn $crate::DesignDyn, target: &prjunnamed_netlist::Value| -> bool {
+            $crate::netlist_replace! { @TOP@ design target [ $($rule)* ] $($rest)* }
         }
     };
     { @TOP@ $design:ident $target:ident $($rest:tt)* } => {
-        let result: Option<Value> = prjunnamed_pattern::netlist_matches! { @TOP@ $design $target $($rest)* };
+        let result: Option<Value> = $crate::netlist_matches! { @TOP@ $design $target $($rest)* };
         if let Some(replace) = result {
             #[allow(unexpected_cfgs)]
             if cfg!(feature = "trace") {
